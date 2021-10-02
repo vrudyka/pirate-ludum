92d89d566dc5119575c64825ca22458ce2244879
Veronika <veronika.rudyka@gmail.com>
2021-10-02 19:24:27 +0300

    Added: Bar.

--- Assets/Scripts/Rooms/Cooking/Bowl.cs
+++ Assets/Scripts/Rooms/Cooking/Bowl.cs
@@ -5,13 +5,37 @@ public class Bowl : MonoBehaviour
 {
     private List<HoldingObjectType> stuffCollected = new List<HoldingObjectType>();
 
+    public int numOfparticles;
+
+    public GameObject minBar;
+    public GameObject modBar;
+    public GameObject maxBar;
+
     public int particles;
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void Awake()
     {
-        Debug.Log("ссс");
+        minBar.transform.localScale = Vector3.zero;
+        modBar.transform.localScale = Vector3.zero;
+        maxBar.transform.localScale = Vector3.zero;
+    }
+    
+    private void Update()
+    {
+        float p = (float)particles / numOfparticles;
 
-
+        if (p < 0.33f)
+        {
+            minBar.transform.localScale = new Vector3(p / 0.33f, 0.5f, 1f);
+        }
+        if (p > 0.33f && p < 0.66f)
+        {
+            modBar.transform.localScale = new Vector3((p - 0.33f) / 0.33f, 0.5f, 1f);
+        }
+        if (p > 0.66f && p < 1f)
+        {
+            maxBar.transform.localScale = new Vector3((p - 0.66f) / 0.33f, 0.5f, 1f);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -34,12 +58,11 @@ public class Bowl : MonoBehaviour
                 sp.Commence();
             }
         }
-        Debug.Log("ueee");
 
         var flyingParticle = collision.gameObject.GetComponent<FlyingParticle>();
         if (flyingParticle != null)
         {
             particles += 1;
-        }     
+        }
     }
 }
